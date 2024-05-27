6cb50351db32266f381ee0b7aed3a279328cdefc
TaufiquRhmn <taufiiqurahman@gmail.com>
2024-05-27 22:55:32 +0700

    update

--- mainapp.py
+++ mainapp.py
@@ -1,46 +1,39 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import tensorflow as tf
-import h5py
+from keras.models import load_model
+from PIL import Image
+import numpy as np
 
-@st.cache(allow_output_mutation=True)
-def load_model():
-  model=tf.keras.models.load_model('/CapstoneProject37/casting.hdf5')
-  return model
-with st.spinner('Model is being loaded..'):
-  model=load_model()
+from util import classify, set_background
 
+
+#set_background('./bgs/bg5.png')
+
+# set title
 st.title('Casting Quality Control')
 
-file = st.file_uploader("Please upload an brain scan file", type=["jpg", "png"])
-import cv2
-from PIL import Image, ImageOps
-import numpy as np
-st.set_option('deprecation.showfileUploaderEncoding', False)
-def import_and_predict(image_data, model):
-    
-        size = (300,300)    
-        image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
-        image = np.asarray(image)
-        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        #img_resize = (cv2.resize(img, dsize=(75, 75),    interpolation=cv2.INTER_CUBIC))/255.
-        
-        img_reshape = img[np.newaxis,...]
-    
-        prediction = model.predict(img_reshape)
-        
-        return prediction
-if file is None:
-    st.text("Please upload an image file")
-else:
-    image = Image.open(file)
+# set header
+st.header('Please upload a Casting Product Image')
+
+# upload file
+file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
+
+# load classifier
+model = load_model('./model/model.h5')
+
+# load class names
+with open('./model/labels.txt', 'r') as f:
+    class_names = [a[:-1].split(' ')[1] for a in f.readlines()]
+    f.close()
+
+
+# display image
+if file is not None:
+    image = Image.open(file).convert('RGB')
     st.image(image, use_column_width=True)
-    predictions = import_and_predict(image, model)
-    score = tf.nn.softmax(predictions[0])
-    st.write(prediction)
-    st.write(score)
-    print(
-    "This image most likely belongs to {} with a {:.2f} percent confidence."
-    .format(class_names[np.argmax(score)], 100 * np.max(score))
-)
+
+    # classify image
+    class_name, conf_score = classify(image, model, class_names)
+
+    # write classification
+    st.write("## {}".format(class_name))
+    st.write("### score: {}%".format(int(conf_score * 1000) / 10))
